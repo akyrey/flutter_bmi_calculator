dd27cc65df72d579e2b57a831b69d59117f3e583
akyrey <stein.dario@gmail.com>
2021-04-13 21:22:13 +0200

    refactor: use const to remove repetitions

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -18,29 +22,34 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(color: activeCardColor),
                 ),
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(color: activeCardColor),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(),
+            child: ReusableCard(color: activeCardColor),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(color: activeCardColor),
                 ),
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(color: activeCardColor),
                 ),
               ],
             ),
           ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            height: bottomContainerHeight,
+          )
         ],
       ),
     );
@@ -48,17 +57,17 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key key,
-  }) : super(key: key);
+  final Widget child;
+  final Color color;
+
+  ReusableCard({@required this.color, this.child});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: child,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: Color(0xFF1D1E33),
-      ),
+          borderRadius: BorderRadius.circular(10.0), color: color),
       margin: EdgeInsets.all(15),
     );
   }
